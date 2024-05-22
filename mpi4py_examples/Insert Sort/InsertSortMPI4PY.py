503feb516c4ea8095abe8ce486f6a872dd5da764
Urmah22 <marco.martinez5073@alumnos.udg.mx>
2024-05-21 23:24:47 -0600

    Update
    Upload new algorithms

--- mpi4py_examples/Insert Sort/InsertSortMPI4PY.py
+++ mpi4py_examples/Insert Sort/InsertSortMPI4PY.py
@@ -1,3 +1,8 @@
+#########    Algoritmo de Insert Sort implementando Mpi4py   ###############
+##
+##
+##              Autor: Martinez Pérez Marco Uriel
+
 from mpi4py import MPI
 
 def insertion_sort(arr):
@@ -5,13 +10,13 @@ def insertion_sort(arr):
     for i in range(1, n):
         current_value = arr[i]  # Guarda el valor actual a insertar
         position = i
-        
+
         # Mueve los elementos del subarreglo ordenado hacia la derecha
         # para hacer espacio para el valor actual
         while position > 0 and arr[position - 1] > current_value:
             arr[position] = arr[position - 1]
             position -= 1
-        
+
         # Inserta el valor actual en la posición correcta
         arr[position] = current_value
 
@@ -34,9 +39,12 @@ if __name__ == "__main__":
     size = comm.Get_size()
 
     if rank == 0:
-        arr = [5, 2, 4, 6, 1, 3]
+        arr = [5, 2, 4, 6, 1, 3, 8, 10, 32, 7, 1, 20, 9, 12]
     else:
         arr = None
+
+    # Registrar el tiempo de inicio
+    start_time = MPI.Wtime()
 
     # Dividir el arreglo en partes iguales
     arr_part = comm.scatter(split_array(arr, size), root=0)
@@ -50,5 +58,7 @@ if __name__ == "__main__":
     # Unir partes ordenadas
     if rank == 0:
         sorted_arr = merge_sorted_parts(sorted_parts)
+        end_time = MPI.Wtime()  # Registrar el tiempo de finalización
         print("Lista original:", arr)
         print("Lista ordenada:", sorted_arr)
+        print(f"Tiempo de ejecución: {end_time - start_time:.8f} segundos")
